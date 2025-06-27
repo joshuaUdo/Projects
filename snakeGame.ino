9cafa8dfd9d21049184a228f8157d61f38c11637
joshuaUdo <anylogin5999@gmail.com>
2025-06-27 14:12:00 +0100

    Update snakeGame.ino

--- snakeGame.ino
+++ snakeGame.ino
@@ -1,45 +1,101 @@
-#include <Adafruit_Neopixel.h>
+#include <Adafruit_NeoPixel.h>
 #define DISPLAY 6
 #define PIXELS 64
 
 #define LEFT 3
 #define RIGHT 4
 
-Adafruit_NeoPixel strip(PIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(PIXELS, DISPLAY, NEO_GRB + NEO_KHZ800);
 
-void Left_Button(){
+unsigned long lastmove = 0;
+const int moveinterval = 400;
 
+struct Point {
+  int x;
+  int y;
+};
+
+Point snake[64];
+int snakeLenght = 3;
+
+int dx = 1;
+int dy = 0;
+
+int getPixelIndex(int x, int y) {
+  if (y % 2 == 0) {
+    return y * 8 + x;
+} else {  //y % 2 != 0
+    return y * 8 + (7 - x);
+  }
 }
 
-void Right_Button(){
+void moveSnake() {
+  for (int i = snakeLenght - 1; i > 0; i--) {
+    snake[i] = snake[i - 1];
+  }
+  snake[0].x += dx;
+  snake[0].y += dy;
 
+  if (snake[0].x > 7) snake[0].x = 0;
+  if (snake[0].x < 0) snake[0].x = 7;
+  if (snake[0].y > 7) snake[0].y = 0;
+  if (snake[0].y < 0) snake[0].y = 7;
+
+  strip.clear();
+
+  //Zeichne die Schlange
+  for (int i = 0; i < snakeLenght; i++) {
+    int index = getPixelIndex(snake[i].x, snake[i].y);
+    strip.setPixelColor(index, strip.Color(0, 255, 0));
+  }
+
+  strip.show();
 }
 
-void checkButton(){
-
+void spawnPoint() {
+  snake[0] = { 3, 4 };  //Head
+  snake[1] = { 2, 4 };  //Body
+  snake[2] = { 1, 4 };  //Tail
 }
 
-void moveSnake(){
-
+void placeFood() {
 }
 
-void placeFood(){
-
+void collision() {
 }
 
-void collision(){
-  
-}
+// void Left_Button(){
+
+// }
+
+// void Right_Button(){
+
+// }
+
+// void button_function(){
+
+// }
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LEFT, INPUT);
-  pinMode(RIGHT, INPUT);
+  pinMode(LEFT, INPUT_PULLUP);
+  pinMode(RIGHT, INPUT_PULLUP);
 
+  strip.begin();
+  strip.clear();
+  strip.setBrightness(100);
+  strip.show();
+
+  spawnPoint();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  unsigned long now = millis();
+  if (now - lastmove >= moveinterval) {
+    moveSnake();
+    lastmove = now;
+  }
 }
